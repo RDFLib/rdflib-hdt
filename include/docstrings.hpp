7a366150cb5e106b9643c04cb0bd9ea70bc2a626
Thomas Minier <thomas.minier@protonmail.com>
2018-01-12 16:10:23 +0100

    semicolons dood

--- include/docstrings.hpp
+++ include/docstrings.hpp
@@ -8,106 +8,104 @@
 
 #include <string>
 
-namespace pyhdtdoc {
-  const std::string MODULE = R"docstring(
-    The hdt module enables to load and query HDT files with ease.
+std::string MODULE = R"docstring(
+  The hdt module enables to load and query HDT files with ease.
 
-    Example:
-    
-    from hdt import HDTDocument
+  Example:
 
-    # Load HDT file. Missing indexes are generated automatically
-    document = HDTDocument("test.hdt")
+  from hdt import HDTDocument
 
-    # Fetch all triples that matches { ?s ?p ?o }
-    (triples, cardinality) = document.search_triples("", "", "")
+  # Load HDT file. Missing indexes are generated automatically
+  document = HDTDocument("test.hdt")
 
-    print("cardinality of { ?s ?p ?o }: %i" % cardinality)
-    for triple in triples:
-      print(triple)
-  )docstring"
+  # Fetch all triples that matches { ?s ?p ?o }
+  (triples, cardinality) = document.search_triples("", "", "")
 
-  const std::string HDT_DOCUMENT_CLASS = R"docstring(
-    An HDTDocument enables to load and query a HDT file.
-    Indexes are automatically generated if missing.
+  print("cardinality of { ?s ?p ?o }: %i" % cardinality)
+  for triple in triples:
+    print(triple)
+)docstring";
 
-    Example:
+std::string HDT_DOCUMENT_CLASS = R"docstring(
+  An HDTDocument enables to load and query a HDT file.
+  Indexes are automatically generated if missing.
 
-    from hdt import HDTDocument
+  Example:
 
-    # Load HDT file. Missing indexes are generated automatically
-    document = HDTDocument("test.hdt")
+  from hdt import HDTDocument
 
-    # Display some metadata about the HDT document itself
-    print("nb triples: %i" % document.get_total_triples())
-    print("nb subjects: %i" % document.get_nb_subjects())
-    print("nb predicates: %i" % document.get_nb_predicates())
-    print("nb objects: %i" % document.get_nb_objets())
-    print("nb shared subject-object: %i" % document.get_nb_shared())
+  # Load HDT file. Missing indexes are generated automatically
+  document = HDTDocument("test.hdt")
 
-    # Fetch all triples that matches { ?s ?p ?o }
-    # Use empty strings ("") to indicates variables
-    (triples, cardinality) = document.search_triples("", "", "")
+  # Display some metadata about the HDT document itself
+  print("nb triples: %i" % document.get_total_triples())
+  print("nb subjects: %i" % document.get_nb_subjects())
+  print("nb predicates: %i" % document.get_nb_predicates())
+  print("nb objects: %i" % document.get_nb_objets())
+  print("nb shared subject-object: %i" % document.get_nb_shared())
 
-    print("cardinality of { ?s ?p ?o }: %i" % cardinality)
-    for triple in triples:
-      print(triple)
+  # Fetch all triples that matches { ?s ?p ?o }
+  # Use empty strings ("") to indicates variables
+  (triples, cardinality) = document.search_triples("", "", "")
 
-    # Search also support limit and offset
-    (triples, cardinality) = document.search_triples("", "", "", limit=10, offset=100)
-    # etc ...
-  )docstring"
+  print("cardinality of { ?s ?p ?o }: %i" % cardinality)
+  for triple in triples:
+    print(triple)
 
-  const std::string HDT_DOCUMENT_GETFILEPATH = R"docstring(
-    Return the path to the HDT file currently loaded
-  )docstring"
+  # Search also support limit and offset
+  (triples, cardinality) = document.search_triples("", "", "", limit=10, offset=100)
+  # etc ...
+)docstring";
 
-  const std::string HDT_DOCUMENT_GETNBTRIPLES = R"docstring(
-    Return the total number of triples in the HDT document
-  )docstring"
+std::string HDT_DOCUMENT_GETFILEPATH = R"docstring(
+  Return the path to the HDT file currently loaded
+)docstring";
 
-  const std::string HDT_DOCUMENT_GETNBSUBJECTS = R"docstring(
-    Return the number of subjects in the HDT document
-  )docstring"
+std::string HDT_DOCUMENT_GETNBTRIPLES = R"docstring(
+  Return the total number of triples in the HDT document
+)docstring";
 
-  const std::string HDT_DOCUMENT_GETNBPREDICATES = R"docstring(
-    Return the number of predicates in the HDT document
-  )docstring"
+std::string HDT_DOCUMENT_GETNBSUBJECTS = R"docstring(
+  Return the number of subjects in the HDT document
+)docstring";
 
-  const std::string HDT_DOCUMENT_GETNBOBJECTS = R"docstring(
-    Return the number of objects in the HDT document
-  )docstring"
+std::string HDT_DOCUMENT_GETNBPREDICATES = R"docstring(
+  Return the number of predicates in the HDT document
+)docstring";
 
-  const std::string HDT_DOCUMENT_GETNBSHARED = R"docstring(
-    Return the number of shared subject-object in the HDT document
-  )docstring"
+std::string HDT_DOCUMENT_GETNBOBJECTS = R"docstring(
+  Return the number of objects in the HDT document
+)docstring";
 
-  const std::string HDT_DOCUMENT_SEARCH_TRIPLES = R"docstring(
-    Search for RDF triples matching the triple pattern { subject predicate object }, with an optional limit and offset.
-    Use empty strings ("") to indicate variables.
+std::string HDT_DOCUMENT_GETNBSHARED = R"docstring(
+  Return the number of shared subject-object in the HDT document
+)docstring";
 
-    Args:
-      subject (str): The subject of the triple pattern to seach for.
-      predicate (str): The predicate of the triple pattern to seach for.
-      obj (str): The object of the triple pattern ot seach for.
-      limit (int, optional): Maximum number of triples to search for.
-      offset (int, optional): Number of matching to skip before returning results.
+std::string HDT_DOCUMENT_SEARCH_TRIPLES = R"docstring(
+  Search for RDF triples matching the triple pattern { subject predicate object }, with an optional limit and offset.
+  Use empty strings ("") to indicate variables.
 
-    Return:
-      A tuple (list of matching triples, triple pattern cardinality).
-      A triple itself is a tuple (subject, predicate, object).
+  Args:
+    subject (str): The subject of the triple pattern to seach for.
+    predicate (str): The predicate of the triple pattern to seach for.
+    obj (str): The object of the triple pattern ot seach for.
+    limit (int, optional): Maximum number of triples to search for.
+    offset (int, optional): Number of matching to skip before returning results.
 
-    Example:
+  Return:
+    A tuple (list of matching triples, triple pattern cardinality).
+    A triple itself is a tuple (subject, predicate, object).
 
-    from hdt import HDTDocument
-    document = HDTDocument("test.hdt")
+  Example:
 
-    (triples, cardinality) = document.search_triples("", "", "")
+  from hdt import HDTDocument
+  document = HDTDocument("test.hdt")
 
-    print("cardinality of { ?s ?p ?o }: %i" % cardinality)
-    for triple in triples:
-      print(triple)
-  )docstring"
-}
+  (triples, cardinality) = document.search_triples("", "", "")
+
+  print("cardinality of { ?s ?p ?o }: %i" % cardinality)
+  for triple in triples:
+    print(triple)
+)docstring";
 
 #endif /* DOCSTRINGS_HPP */
--- src/hdt.cpp
+++ src/hdt.cpp
@@ -5,20 +5,19 @@
 #include "hdt_document.hpp"
 
 namespace py = pybind11;
-namespace doc = pyhdtdoc;
 
 PYBIND11_MODULE(hdt, m) {
-    m.doc() = doc::MODULE;
+    m.doc() = MODULE;
 
-    py::class_<HDTDocument>(m, "HDTDocument", doc::HDT_DOCUMENT_CLASS)
+    py::class_<HDTDocument>(m, "HDTDocument", HDT_DOCUMENT_CLASS)
       .def(py::init(&HDTDocument::create))
-      .def("get_file_path", &HDTDocument::getFilePath, doc::HDT_DOCUMENT_GETFILEPATH)
-      .def("get_total_triples", &HDTDocument::getNbTriples, doc::HDT_DOCUMENT_GETNBTRIPLES)
-      .def("get_nb_subjects", &HDTDocument::getNbSubjects, doc::HDT_DOCUMENT_GETNBSUBJECTS)
-      .def("get_nb_predicates", &HDTDocument::getNbPredicates, doc::HDT_DOCUMENT_GETNBPREDICATES)
-      .def("get_nb_objects", &HDTDocument::getNbObjects, doc::HDT_DOCUMENT_GETNBOBJECTS)
-      .def("get_nb_shared", &HDTDocument::getNbShared, doc::HDT_DOCUMENT_GETNBSHARED)
-      .def("search_triples", &HDTDocument::search, doc::HDT_DOCUMENT_SEARCH_TRIPLES,
+      .def("get_file_path", &HDTDocument::getFilePath, HDT_DOCUMENT_GETFILEPATH)
+      .def("get_total_triples", &HDTDocument::getNbTriples, HDT_DOCUMENT_GETNBTRIPLES)
+      .def("get_nb_subjects", &HDTDocument::getNbSubjects, HDT_DOCUMENT_GETNBSUBJECTS)
+      .def("get_nb_predicates", &HDTDocument::getNbPredicates, HDT_DOCUMENT_GETNBPREDICATES)
+      .def("get_nb_objects", &HDTDocument::getNbObjects, HDT_DOCUMENT_GETNBOBJECTS)
+      .def("get_nb_shared", &HDTDocument::getNbShared, HDT_DOCUMENT_GETNBSHARED)
+      .def("search_triples", &HDTDocument::search, HDT_DOCUMENT_SEARCH_TRIPLES,
           py::arg("subject"), py::arg("predicate"), py::arg("object"), py::arg("limit") = 0, py::arg("offset") = 0)
       .def("__repr__", &HDTDocument::python_repr);
 

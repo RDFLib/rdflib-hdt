743f64349f649e46aefa68790800027734074c74
Thomas Minier <tminier01@gmail.com>
2018-12-15 10:44:43 +0100

    rework tests

--- tests/hdt_document_test.py
+++ tests/hdt_document_test.py
@@ -1,46 +1,51 @@
 # hdt_document_test.py
 # Author: Thomas MINIER - MIT License 2017-2018
+import pytest
 from hdt import HDTDocument
-import unittest
 
 path = "tests/test.hdt"
 document = HDTDocument(path)
 nbTotalTriples = 132
 
 
-class TestHDTDocument(unittest.TestCase):
+def test_missing_file():
+    with pytest.raises(RuntimeError):
+        HDTDocument("/home/dtrump/wall.hdt")
 
-    @unittest.expectedFailure
-    def test_missing_file(self):
-        wrongDoc = HDTDocument("/home/dtrump/wall.hdt")
 
-    def test_file_path(self):
-        self.assertEqual(document.file_path, path)
+def test_file_path():
+    assert document.file_path == path
 
-    def test_total_triples(self):
-        self.assertEqual(document.total_triples, nbTotalTriples)
-        self.assertEqual(len(document), nbTotalTriples)
 
-    def test_nb_subjects(self):
-        self.assertEqual(document.nb_subjects, 4)
+def test_total_triples():
+    assert document.total_triples == nbTotalTriples
+    assert len(document) == nbTotalTriples
 
-    def tests_nb_predicates(self):
-        self.assertEqual(document.nb_predicates, 3)
 
-    def tests_nb_objects(self):
-        self.assertEqual(document.nb_objects, 112)
+def test_nb_subjects():
+    assert document.nb_subjects == 4
 
-    def tests_nb_shared(self):
-        self.assertEqual(document.nb_shared, 0)
 
-    def test_ids_to_string(self):
-        (triples, triplesCard) = document.search_triples("", "", "")
-        (ids, idsCard) = document.search_triples_ids("", "", "")
-        self.assertEqual(triplesCard, idsCard)
-        self.assertEqual(triplesCard, nbTotalTriples)
-        for subj, pred, obj in triples:
-            sid, pid, oid = next(ids)
-            s, p, o = document.tripleid_to_string(sid, pid, oid)
-            self.assertEqual(subj, s)
-            self.assertEqual(pred, p)
-            self.assertEqual(obj, o)
+def tests_nb_predicates():
+    assert document.nb_predicates == 3
+
+
+def tests_nb_objects():
+    assert document.nb_objects == 112
+
+
+def tests_nb_shared():
+    assert document.nb_shared == 0
+
+
+def test_ids_to_string():
+    (triples, triplesCard) = document.search_triples("", "", "")
+    (ids, idsCard) = document.search_triples_ids("", "", "")
+    assert triplesCard == idsCard
+    assert triplesCard == nbTotalTriples
+    for subj, pred, obj in triples:
+        sid, pid, oid = next(ids)
+        s, p, o = document.tripleid_to_string(sid, pid, oid)
+        assert subj == s
+        assert pred == p
+        assert obj == o
--- tests/hdt_iterators_test.py
+++ tests/hdt_iterators_test.py
@@ -1,94 +1,99 @@
 # hdt_iterators_test.py
 # Author: Thomas MINIER - MIT License 2017-2018
 from hdt import HDTDocument
-import unittest
 
 path = "tests/test.hdt"
 document = HDTDocument(path)
 nbTotalTriples = 132
 
 
-class TestHDTIterators(unittest.TestCase):
-    def test_read_document_base(self):
-        (triples, cardinality) = document.search_triples("", "", "")
-        self.assertEqual(triples.subject, "?s")
-        self.assertEqual(triples.predicate, "?p")
-        self.assertEqual(triples.object, "?o")
-        self.assertEqual(cardinality, nbTotalTriples)
-        for subj, pred, obj in triples:
-            self.assertNotEqual(subj, None)
-            self.assertNotEqual(pred, None)
-            self.assertNotEqual(obj, None)
-        self.assertEqual(triples.nb_reads, cardinality)
+def test_read_document_base():
+    (triples, cardinality) = document.search_triples("", "", "")
+    assert triples.subject == "?s"
+    assert triples.predicate == "?p"
+    assert triples.object == "?o"
+    assert cardinality == nbTotalTriples
+    for subj, pred, obj in triples:
+        assert subj is not None
+        assert pred is not None
+        assert obj is not None
+    assert triples.nb_reads == cardinality
 
-    def test_read_document_limit(self):
-        nbItems = 0
-        (triples, cardinality) = document.search_triples("", "", "", limit=10)
-        self.assertEqual(triples.limit, 10)
-        self.assertEqual(cardinality, nbTotalTriples)
-        for subj, pred, obj in triples:
-            nbItems += 1
-            self.assertNotEqual(subj, None)
-            self.assertNotEqual(pred, None)
-            self.assertNotEqual(obj, None)
-        self.assertEqual(nbItems, 10)
-        self.assertEqual(triples.nb_reads, 10)
 
-    def test_read_document_offset(self):
-        nbItems = 0
-        (triples, cardinality) = document.search_triples("", "", "", offset=10)
-        self.assertEqual(triples.offset, 10)
-        self.assertEqual(cardinality, nbTotalTriples)
-        for subj, pred, obj in triples:
-            nbItems += 1
-            self.assertNotEqual(subj, None)
-            self.assertNotEqual(pred, None)
-            self.assertNotEqual(obj, None)
-        self.assertEqual(nbItems, cardinality - 10)
-        self.assertEqual(triples.nb_reads, cardinality - 10)
+def test_read_document_limit():
+    nbItems = 0
+    (triples, cardinality) = document.search_triples("", "", "", limit=10)
+    assert triples.limit == 10
+    assert cardinality == nbTotalTriples
+    for subj, pred, obj in triples:
+        nbItems += 1
+        assert subj is not None
+        assert pred is not None
+        assert obj is not None
+    assert nbItems == 10
+    assert triples.nb_reads == 10
 
-    def test_read_document_ids(self):
-        (triples, cardinality) = document.search_triples_ids("", "", "")
-        self.assertEqual(triples.subject, "?s")
-        self.assertEqual(triples.predicate, "?p")
-        self.assertEqual(triples.object, "?o")
-        self.assertEqual(cardinality, nbTotalTriples)
-        for subj, pred, obj in triples:
-            self.assertTrue(subj > 0)
-            self.assertTrue(pred > 0)
-            self.assertTrue(obj > 0)
-        self.assertEqual(triples.nb_reads, cardinality)
 
-    def test_string_iterator_peek(self):
-        expected = ('http://example.org/s1', 'http://example.org/p1', 'http://example.org/o001')
-        (triples, cardinality) = document.search_triples("", "", "")
-        v = triples.peek()
-        self.assertEqual(v, expected)
-        self.assertEqual(triples.nb_reads, 0)
-        v = next(triples)
-        self.assertEqual(v, expected)
-        self.assertEqual(triples.nb_reads, 1)
+def test_read_document_offset():
+    nbItems = 0
+    (triples, cardinality) = document.search_triples("", "", "", offset=10)
+    assert triples.offset == 10
+    assert cardinality == nbTotalTriples
+    for subj, pred, obj in triples:
+        nbItems += 1
+        assert subj is not None
+        assert pred is not None
+        assert obj is not None
+    assert nbItems == cardinality - 10
+    assert triples.nb_reads == cardinality - 10
 
-    def test_ids_iterator_peek(self):
-        expected = (1, 1, 13)
-        (triples, cardinality) = document.search_triples_ids("", "", "")
-        v = triples.peek()
-        self.assertEqual(v, expected)
-        self.assertEqual(triples.nb_reads, 0)
-        v = next(triples)
-        self.assertEqual(v, expected)
-        self.assertEqual(triples.nb_reads, 1)
 
-    def test_string_iterator_big_offset(self):
-        nbItems = 0
-        (triples, cardinality) = document.search_triples("", "", "", offset=nbTotalTriples + 1)
-        for s, p, o in triples:
-            nbItems += 1
-        self.assertEqual(nbItems, 0)
+def test_read_document_ids():
+    (triples, cardinality) = document.search_triples_ids("", "", "")
+    assert triples.subject == "?s"
+    assert triples.predicate == "?p"
+    assert triples.object, "?o"
+    assert cardinality, nbTotalTriples
+    for subj, pred, obj in triples:
+        assert subj is not None
+        assert pred is not None
+        assert obj is not None
+    assert triples.nb_reads == cardinality
 
-    def test_ids_iterator_big_offset(self):
-        nbItems = 0
-        (triples, cardinality) = document.search_triples_ids("", "", "", offset=nbTotalTriples + 1)
-        for s, p, o in triples:
-            nbItems += 1
-        self.assertEqual(nbItems, 0)
+
+def test_string_iterator_peek():
+    expected = ('http://example.org/s1', 'http://example.org/p1', 'http://example.org/o001')
+    (triples, cardinality) = document.search_triples("", "", "")
+    v = triples.peek()
+    assert v == expected
+    assert triples.nb_reads == 0
+    v = next(triples)
+    assert v == expected
+    assert triples.nb_reads == 1
+
+
+def test_ids_iterator_peek():
+    expected = (1, 1, 13)
+    (triples, cardinality) = document.search_triples_ids("", "", "")
+    v = triples.peek()
+    assert v == expected
+    assert triples.nb_reads == 0
+    v = next(triples)
+    assert v == expected
+    assert triples.nb_reads == 1
+
+
+def test_string_iterator_big_offset():
+    nbItems = 0
+    (triples, cardinality) = document.search_triples("", "", "", offset=nbTotalTriples + 1)
+    for s, p, o in triples:
+        nbItems += 1
+    assert nbItems == 0
+
+
+def test_ids_iterator_big_offset():
+    nbItems = 0
+    (triples, cardinality) = document.search_triples_ids("", "", "", offset=nbTotalTriples + 1)
+    for s, p, o in triples:
+        nbItems += 1
+    assert nbItems == 0

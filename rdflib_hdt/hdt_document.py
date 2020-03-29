7323c95044ac7e8111a296e5d10b0d4f7b5a86f6
Thomas Minier <tminier01@gmail.com>
2020-03-29 10:22:21 +0200

    add integration tests for HDTStore & fix a bug in HDTDocument wrapper

--- rdflib_hdt/hdt_document.py
+++ rdflib_hdt/hdt_document.py
@@ -13,6 +13,7 @@ SearchQuery = Tuple[Optional[Term], Optional[Term], Optional[Term]]
 
 
 def term_to_rdflib(term: str) -> Term:
+    """Convert an HDT term into its RDFlib representation"""
     if term.startswith("\""):
         return from_n3(term)
     else:
@@ -20,8 +21,17 @@ def term_to_rdflib(term: str) -> Term:
 
 
 def triple_to_rdflib(triple: Tuple[str, str, str]) -> Tuple[Term, Term, Term]:
+    """Convert an HDT Triple into its RDFlib representation"""
     s, p, o = triple
     return (term_to_rdflib(s), term_to_rdflib(p), term_to_rdflib(o))
+
+
+def rdflib_to_hdt(term: Term) -> str:
+    """Convert an RDFlib term into an HDT representation"""
+    value = term.n3()
+    if value.startswith('<') and value.endswith('>'):
+        return value[1: len(value) - 1]
+    return value
 
 
 class HDTDocument(hdt.HDTDocument):
@@ -52,8 +62,8 @@ class HDTDocument(hdt.HDTDocument):
             A 2-elements tuple (iterator, estimated pattern cardinality), where
             the iterator is a generator of matching RDF triples. A RDF triple itself is a 3-elements tuple (subject, predicate, object) of RDF terms (in rdflib format).
         """
-        subj = query[0] if query[0] is not None else ""
-        pred = query[1] if query[1] is not None else ""
-        obj = query[2] if query[2] is not None else ""
+        subj = rdflib_to_hdt(query[0]) if query[0] is not None else ""
+        pred = rdflib_to_hdt(query[1]) if query[1] is not None else ""
+        obj = rdflib_to_hdt(query[2]) if query[2] is not None else ""
         triples, cardinality = super().search_triples(subj, pred, obj, limit=limit, offset=offset)
         return map(triple_to_rdflib, triples), cardinality
--- tests/hdt_store_test.py
+++ tests/hdt_store_test.py
@@ -6,15 +6,63 @@ from rdflib import Graph, URIRef, Literal
 
 path = "tests/test.hdt"
 store = HDTStore(path)
-expected_length = 128
+
+fixtures = [
+    # pattern ?s ?p ?o
+    ((None, None, None), 128),
+    # pattern s p o
+    ((URIRef('http://example.org/s1'), URIRef('http://example.org/p1'), URIRef('http://example.org/o002')), 1),
+    ((URIRef('http://example.org/s5'), URIRef('http://example.org/p1'), URIRef('http://example.org/o002')), 0),
+    # pattern s ?p ?o
+    ((URIRef('http://example.org/s1'), None, None), 100),
+    ((URIRef('http://example.org/s2'), None, None), 10),
+    ((URIRef('http://example.org/s3'), None, None), 10),
+    ((URIRef('http://example.org/s4'), None, None), 8),
+    ((URIRef('http://example.org/s5'), None, None), 0),
+    # pattern ?s p ?o
+    ((None, URIRef('http://example.org/p1'), None), 110),
+    ((None, URIRef('http://example.org/p2'), None), 10),
+    ((None, URIRef('http://example.org/p3'), None), 8),
+    ((None, URIRef('http://example.org/p99'), None), 0),
+    # pattern ?s ?p o
+    ((None, None, URIRef('http://example.org/o002')), 3),
+    ((None, None, URIRef('http://example.org/o004')), 3),
+    ((None, None, Literal('a')), 1),
+    ((None, None, Literal('a', lang='en')), 1),
+    ((None, None, Literal('', lang='en')), 1),
+    ((None, None, Literal('', datatype=URIRef('http://example.org/literal'))), 1),
+    ((None, None, URIRef('http://example.org/o999')), 0),
+    # pattern s ?p o
+    ((URIRef('http://example.org/s1'), None, URIRef('http://example.org/o002')), 1),
+    ((URIRef('http://example.org/s2'), None, URIRef('http://example.org/o004')), 1),
+    ((URIRef('http://example.org/s3'), None, URIRef('http://example.org/o004')), 1),
+    ((URIRef('http://example.org/s99'), None, URIRef('http://example.org/o004')), 0),
+    # pattern s p ?o
+    ((URIRef('http://example.org/s1'), URIRef('http://example.org/p1'), None), 100),
+    ((URIRef('http://example.org/s2'), URIRef('http://example.org/p1'), None), 10),
+    ((URIRef('http://example.org/s3'), URIRef('http://example.org/p2'), None), 10),
+    ((URIRef('http://example.org/s3'), URIRef('http://example.org/p999'), None), 0),
+    # pattern ?s p o
+    ((None, URIRef('http://example.org/p1'), URIRef('http://example.org/o002')), 2),
+    ((None, URIRef('http://example.org/p2'), URIRef('http://example.org/o004')), 1),
+    ((None, URIRef('http://example.org/p2'), URIRef('http://example.org/o999')), 0)
+]
 
 
-def test_rdflib_graph_basic():
+@pytest.mark.parametrize("query,expected_length", fixtures)
+def test_rdflib_graph_search(query, expected_length):
+    query_s, query_p, query_o = query
     graph = Graph(store=store)
     nb_triples = 0
-    for s, p, o in graph.triples((None, None, None)):
+    for s, p, o in graph.triples(query):
         nb_triples += 1
-        assert(isinstance(s, URIRef))
-        assert(isinstance(p, URIRef))
-        assert(isinstance(o, URIRef) or isinstance(o, Literal))
-    assert(nb_triples == expected_length)
+        assert isinstance(s, URIRef), f"The subject of an RDF triple must be an URI"
+        assert isinstance(p, URIRef), f"The predicate of an RDF triple must be an URI"
+        assert isinstance(o, URIRef) or isinstance(o, Literal), f"The object of an RDF triple must be an URI or a Literal"
+        if query_s is not None:
+            assert s == query_s, f"The expected RDF triple subject's is {query_s}"
+        if query_p is not None:
+            assert p == query_p, f"The expected RDF triple predicate's is {query_p}"
+        if query_o is not None:
+            assert o == query_o, f"The expected RDF triple subject's is {query_o}"
+    assert nb_triples == expected_length, f"The expected number of matches for {query} is {expected_length}"
